b1611ecc00dd050856559460348e200dcb2d847b
Yerannnnnn <yuanjiuzheng@foxmail.com>
2020-05-28 12:53:01 +0800

    Site updated: 2020-05-28 12:52:58

--- live2d_widget/autoload.js
+++ live2d_widget/autoload.js
@@ -34,8 +34,8 @@ function loadExternalResource(url, type) {
 	Promise.all([
 		loadExternalResource(live2d_path + "waifu.css", "css"),
 		// loadExternalResource(live2d_path + "live2d.min.js", "js"),
-        loadExternalResource(live2d_path + "live2d.ny.min.js", "js"),
-		loadExternalResource(live2d_path + "waifu-tips.min.js", "js")
+        loadExternalResource(live2d_path + "live2d.ny.js", "js"),
+		loadExternalResource(live2d_path + "waifu-tips.js", "js")
 	]).then(() => {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.js",
